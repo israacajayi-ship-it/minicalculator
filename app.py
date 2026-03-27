65535b4be4d1a9645d02695b4a839c8b1759ceaf
isaac,israelcodes <israacajayi@gmail.com>
2026-03-27 18:42:50 +0000

    initial commit of israel and isacc codes

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.title("simple addition app")
+# st.write("my favourite game")
+# st.write("my favourite verse")
+# st.write("my favourite app")
+
+num1=st.number_input("enter first number")
+num2=st.number_input("enter second number")
+
+
+if st.button("Add"):
+    result=num1+num2
+    st.write("Answer:", result)
+
+
+
+
+
+
+
+
---
+++ isaac.py
@@ -0,0 +1,17 @@
+# a = 12
+# b = 14
+# c =a+b
+# print(c)
+name=input("what is your name?") 
+print ("your name is ",name,) \
+
+
+
+
+
+
+
+
+
+
+
