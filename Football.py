d460e97aaed380ee85d861331daa8d9c7eff759c
isaac,israelcodes <israacajayi@gmail.com>
2026-04-14 19:16:13 +0100

    new commit added

---
+++ Football.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.title("Welcome to our website")
+st.header("About site")
+st.write("Our website is going to be about football info like:teams progession, player injuries ,manager sacked,relegation battle and early line-ups notification ")
+st.image("footballwebsite.png")
+
+
+
+
+
+
+
+
+
+
+
--- site.py
+++ site.py
@@ -2,12 +2,12 @@ import streamlit as st
 st.title("Welcome to our website")
 st.header("About us")
 st.write("my name is Israel and the other one is Isaac, We both like football but he likes food more than me")
-st.image ("isaacisrael.png")
+st.image ("isaacisrael.png",width=350)
 
 st.header ("Hobbies")
 st.write("Football,Cooking,reading,laughing.")
-st.image("israelscoresisaacwatched.png")
-st.image("israelcookisaacsmelledit.png")
+st.image("israelscoresisaacwatched.png",width=150)
+st.image("israelcookisaacsmelledit.png",width=150)
 
 
 
